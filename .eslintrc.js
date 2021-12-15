44fd47c01f401fe7918697655b09a6ce7ea484cb
Jesus Rivera <jgrcreations@gmail.com>
2021-12-15 14:44:07 -0500

    se arreglo los problemas y las pruevas

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'standard',
-    'plugin:react/jsx-runtime'
+    'standard'
   ],
   parserOptions: {
     ecmaFeatures: {
